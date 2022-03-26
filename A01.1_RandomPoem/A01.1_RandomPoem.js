4f620002eed70c3b71ab02da43e86bd104231f07
RWongMKB <92439984+RWong98@users.noreply.github.com>
2022-03-26 23:27:33 +0100

    Update Aufgabe 1.1
    for-Schleife zählt herunter statt hoch

--- A01.1_RandomPoem/A01.1_RandomPoem.js
+++ A01.1_RandomPoem/A01.1_RandomPoem.js
@@ -3,12 +3,14 @@ var RandomPoem;
     var subject = ["Isaac Clarke", "Dante", "Sebastian Castellanos", "Simon Jarrett", "Six", "Kratos", "Nathan Drake", "Heather Mason", "Hugh Jass", "Subject Delta", "Bully Maguire", "Ash Williams"];
     var predicate = ["shoots", "defeats", "runs from", "experiences an existential crisis and dread with", "karate-chops", "throws", "twerks on", "laughs at", "slays", "farts in the general direction of", "dances with", "sings with"];
     var object = ["Necromorphs.", "Demons.", "Deadites.", "a human conciousness in a machine body.", "Valkyries.", "me.", "you.", "Splicers.", "EIA2.", "butts.", "SCP-504.", "Marvin, the paranoid Android."];
+    /* Funktion zum erstellen einer random Zahl */
     function makeRandom() {
         return Math.floor(Math.random() * subject.length);
     }
     getVerse(subject, predicate, object);
+    /* Funktion zum erstellen eines kurzen poems aus Subjekt, Prädikat und Objekt */
     function getVerse(_subject, _predicate, _object) {
-        for (var i = 0; i < 12; i++) {
+        for (var i = subject.length; i > 0; i--) {
             var chosensubject = makeRandom();
             var chosenpredicate = makeRandom();
             var chosenobject = makeRandom();
--- A01.1_RandomPoem/A01.1_RandomPoem.ts
+++ A01.1_RandomPoem/A01.1_RandomPoem.ts
@@ -3,15 +3,16 @@ namespace RandomPoem {
   let predicate: string[] = ["shoots", "defeats", "runs from", "experiences an existential crisis and dread with", "karate-chops", "throws", "twerks on", "laughs at", "slays", "farts in the general direction of", "dances with", "sings with"];
   let object: string[] = ["Necromorphs.", "Demons.", "Deadites.", "a human conciousness in a machine body.", "Valkyries.", "me.", "you.", "Splicers.", "EIA2.", "butts.", "SCP-504.", "Marvin, the paranoid Android."];
 
+  /* Funktion zum erstellen einer random Zahl */
   function makeRandom(): number {
     return Math.floor(Math.random() * subject.length);
   }
 
   getVerse(subject, predicate, object);
-  
 
+  /* Funktion zum erstellen eines kurzen poems aus Subjekt, Prädikat und Objekt */
   function getVerse(_subject: string[], _predicate: string[], _object: string[]): void {
-    for (let i: number = 0; i < 12; i++) {
+    for (let i: number = subject.length; i > 0; i--) {
 
       let chosensubject: number = makeRandom();
       let chosenpredicate: number = makeRandom();
