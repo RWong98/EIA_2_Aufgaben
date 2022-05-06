daf9d75680a942f5d1c3c3cf4f21f0f55994919b
RWongMKB <92439984+RWong98@users.noreply.github.com>
2022-05-06 21:53:03 +0200

    A03 basic game added
    + colours
    - amount
    - size
    - font

--- A03_MemorySettings/A03.js
+++ A03_MemorySettings/A03.js
@@ -1,7 +1,73 @@
+/*
+Aufgabe: 03.2 Memory Settings
+Name: Ricco Wong
+Matrikel: 269596
+Quellen: mit Hilfe von Silvan
+*/
 var MemoryGame;
 (function (MemoryGame) {
-    function handleChange(_event) {
-        var target = _event.target;
+    window.addEventListener("load", handleLoad);
+    var allCards = ["1", "1", "2", "2", "3", "3", "4", "4", "5", "5", "6", "6", "7", "7", "8", "8", "9", "9", "10", "10",
+        "11", "11", "12", "12", "13", "13", "14", "14", "15", "15", "16", "16", "17", "17", "18", "18", "19", "19", "20", "20", "21", "21", "22", "22",
+        "23", "23", "24", "24", "25", "25", "26", "26", "27", "27", "28", "28"];
+    var newCards = [];
+    var colorSelector;
+    var cardColor;
+    function handleLoad(_event) {
+        document.getElementById("StartGame").addEventListener("click", startGame);
+        colorSelector = document.querySelector("#color");
+    }
+    function startGame() {
+        cardColor = colorSelector.value;
+        shuffleArray(allCards);
+    }
+    function shuffleArray(_allCards) {
+        var randomCard;
+        while (_allCards.length != 0) {
+            randomCard = Math.floor(Math.random() * _allCards.length);
+            newCards.push(_allCards[randomCard]);
+            createCard(_allCards[randomCard]);
+            _allCards.splice(randomCard, 1);
+        }
+        return newCards;
+    }
+    function createCard(_cardValue) {
+        var card = document.createElement("div");
+        card.style.backgroundColor = cardColor;
+        card.style.color = cardColor;
+        card.addEventListener("click", function () { showCard(card); });
+        var field = document.getElementById("cardField");
+        card.innerHTML = _cardValue;
+        card.className = "Card " + _cardValue;
+        field.appendChild(card);
+    }
+    var oneActiveCard = false;
+    var currentCards = [];
+    function showCard(_card) {
+        _card.style.color = "white";
+        currentCards.push(_card);
+        if (oneActiveCard == false) {
+            oneActiveCard = true;
+        }
+        else {
+            document.querySelector("body").style.pointerEvents = "none";
+            setTimeout(startTimer, 2000);
+            oneActiveCard = false;
+        }
+    }
+    function hideCards() {
+        currentCards[0].style.color = cardColor;
+        currentCards[1].style.color = cardColor;
+        console.log(currentCards);
+        if (currentCards[0].className == currentCards[1].className) {
+            currentCards[0].style.visibility = "hidden";
+            currentCards[1].style.visibility = "hidden";
+        }
+        currentCards = [];
+        document.querySelector("body").style.pointerEvents = "";
+    }
+    function startTimer() {
+        hideCards();
     }
 })(MemoryGame || (MemoryGame = {}));
 // Work in Progress
--- A03_MemorySettings/A03.ts
+++ A03_MemorySettings/A03.ts
@@ -1,6 +1,85 @@
+/*
+Aufgabe: 03.2 Memory Settings
+Name: Ricco Wong
+Matrikel: 269596
+Quellen: mit Hilfe von Silvan
+*/
+
 namespace MemoryGame {
-    function handleChange(_event) {
-        let target = _event.target;
+    window.addEventListener("load", handleLoad);
+
+    let allCards: string[] = ["1", "1", "2", "2", "3", "3", "4", "4", "5", "5", "6", "6", "7", "7", "8", "8", "9", "9", "10", "10",
+        "11", "11", "12", "12", "13", "13", "14", "14", "15", "15", "16", "16", "17", "17", "18", "18", "19", "19", "20", "20", "21", "21", "22", "22",
+        "23", "23", "24", "24", "25", "25", "26", "26", "27", "27", "28", "28"];
+    let newCards: string[] = [];
+    let colorSelector: HTMLInputElement;
+    let cardColor: string;
+
+    function handleLoad(_event: Event): void {
+        document.getElementById("StartGame").addEventListener("click", startGame);
+        colorSelector = <HTMLInputElement>document.querySelector("#color");
     }
+    function startGame(): void {
+        cardColor = colorSelector.value;
+        shuffleArray(allCards);
+    }
+
+    function shuffleArray(_allCards: string[]): string[] {
+        let randomCard: number;
+        while (_allCards.length != 0) {
+            randomCard = Math.floor(Math.random() * _allCards.length);
+            newCards.push(_allCards[randomCard]);
+            createCard(_allCards[randomCard]);
+            _allCards.splice(randomCard, 1);
+
+        }
+        return newCards;
+    }
+
+    function createCard(_cardValue: string): void {
+        let card: HTMLDivElement = document.createElement("div");
+        card.style.backgroundColor = cardColor;
+        card.style.color = cardColor;
+        card.addEventListener("click", () => { showCard(card); });
+        let field: HTMLElement = document.getElementById("cardField");
+        card.innerHTML = _cardValue;
+        card.className = "Card " + _cardValue;
+        field.appendChild(card);
+
+    }
+
+    let oneActiveCard: boolean = false;
+    let currentCards: HTMLDivElement[] = [];
+
+
+    function showCard(_card: HTMLDivElement): void {
+        _card.style.color = "white";
+        currentCards.push(_card);
+        if (oneActiveCard == false) {
+            oneActiveCard = true;
+        }
+        else {
+            document.querySelector("body").style.pointerEvents = "none";
+            setTimeout(startTimer, 2000);
+            oneActiveCard = false;
+        }
+    }
+
+    function hideCards(): void {
+        currentCards[0].style.color = cardColor;
+        currentCards[1].style.color = cardColor;
+        console.log(currentCards);
+        if (currentCards[0].className == currentCards[1].className) {
+            currentCards[0].style.visibility = "hidden";
+            currentCards[1].style.visibility = "hidden";
+        }
+        currentCards = [];
+        document.querySelector("body").style.pointerEvents = "";
+    }
+
+    function startTimer(): void {
+        hideCards();
+    }
+
 }
 // Work in Progress
