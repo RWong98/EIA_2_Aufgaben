ea9113a7417ffbe09625b6cb9e9557c11e467aa1
RWongMKB <92439984+RWong98@users.noreply.github.com>
2022-05-07 13:21:17 +0200

    A03 Update
    Everything

--- A03_MemorySettings/A03.js
+++ A03_MemorySettings/A03.js
@@ -9,17 +9,52 @@ var MemoryGame;
     window.addEventListener("load", handleLoad);
     var allCards = ["1", "1", "2", "2", "3", "3", "4", "4", "5", "5", "6", "6", "7", "7", "8", "8", "9", "9", "10", "10",
         "11", "11", "12", "12", "13", "13", "14", "14", "15", "15", "16", "16", "17", "17", "18", "18", "19", "19", "20", "20", "21", "21", "22", "22",
-        "23", "23", "24", "24", "25", "25", "26", "26", "27", "27", "28", "28"];
+        "23", "23", "24", "24", "25", "25"];
     var newCards = [];
+    var body;
     var colorSelector;
     var cardColor;
+    var bodyColorSelector;
+    var fontColor;
+    var fontColorSelector;
+    var sizeSelector;
+    var cardSize;
+    var chosenCards = [];
+    var stepperSelector;
+    var cardFont;
+    var minutes = 0;
+    var seconds = 0;
     function handleLoad(_event) {
         document.getElementById("StartGame").addEventListener("click", startGame);
         colorSelector = document.querySelector("#color");
+        bodyColorSelector = document.querySelector("#bodycolor");
+        fontColorSelector = document.querySelector("#fontcolor");
+        sizeSelector = document.querySelector("#slider");
+        stepperSelector = document.querySelector("#stepper");
+        body = document.querySelector("body");
+    }
+    function radioEntry() {
+        var formData = new FormData(document.forms[0]);
+        if (formData.get("radio") != null && formData.get("radio") != undefined) {
+            cardFont = (formData.get("radio") + "");
+        }
     }
     function startGame() {
         cardColor = colorSelector.value;
-        shuffleArray(allCards);
+        body.style.backgroundColor = bodyColorSelector.value;
+        fontColor = fontColorSelector.value;
+        cardSize = sizeSelector.value;
+        chooseCards();
+        radioEntry();
+        shuffleArray(chosenCards);
+        body.removeChild(document.querySelector("#menu"));
+        setTimer();
+    }
+    function chooseCards() {
+        for (var i = 0; i < parseInt(stepperSelector.value); i++) {
+            chosenCards.push(allCards[i]);
+        }
+        console.log(chosenCards);
     }
     function shuffleArray(_allCards) {
         var randomCard;
@@ -29,12 +64,16 @@ var MemoryGame;
             createCard(_allCards[randomCard]);
             _allCards.splice(randomCard, 1);
         }
+        console.log(newCards);
         return newCards;
     }
     function createCard(_cardValue) {
         var card = document.createElement("div");
         card.style.backgroundColor = cardColor;
         card.style.color = cardColor;
+        card.style.padding = cardSize + "px";
+        card.style.fontFamily = cardFont;
+        console.log(cardFont);
         card.addEventListener("click", function () { showCard(card); });
         var field = document.getElementById("cardField");
         card.innerHTML = _cardValue;
@@ -44,7 +83,9 @@ var MemoryGame;
     var oneActiveCard = false;
     var currentCards = [];
     function showCard(_card) {
-        _card.style.color = "white";
+        _card.style.color = fontColor;
+        _card.style.pointerEvents = "none";
+        _card.style.backgroundColor = "blue";
         currentCards.push(_card);
         if (oneActiveCard == false) {
             oneActiveCard = true;
@@ -56,19 +97,39 @@ var MemoryGame;
         }
     }
     function hideCards() {
+        currentCards[0].style.backgroundColor = cardColor;
+        currentCards[1].style.backgroundColor = cardColor;
         currentCards[0].style.color = cardColor;
         currentCards[1].style.color = cardColor;
         console.log(currentCards);
         if (currentCards[0].className == currentCards[1].className) {
             currentCards[0].style.visibility = "hidden";
             currentCards[1].style.visibility = "hidden";
+            currentCards[0].setAttribute("deleted", "deleted");
+            currentCards[1].setAttribute("deleted", "deleted");
+            if (document.querySelectorAll("div div").length == document.querySelectorAll("[deleted = 'deleted']").length) {
+                alert("Du hast gewonnen! In " + minutes + " Minuten und " + seconds + " Sekunden!");
+                location.reload();
+            }
         }
+        currentCards[0].style.pointerEvents = "";
+        currentCards[1].style.pointerEvents = "";
         currentCards = [];
         document.querySelector("body").style.pointerEvents = "";
+    }
+    function setTimer() {
+        setInterval(timer, 1000);
+    }
+    function timer() {
+        seconds++;
+        if (seconds == 60) {
+            seconds = 0;
+            minutes++;
+        }
+        document.querySelector("#timer").innerHTML = minutes + ":" + seconds;
     }
     function startTimer() {
         hideCards();
     }
 })(MemoryGame || (MemoryGame = {}));
-// Work in Progress
 //# sourceMappingURL=A03.js.map
--- A03_MemorySettings/A03.ts
+++ A03_MemorySettings/A03.ts
@@ -10,19 +10,59 @@ namespace MemoryGame {
 
     let allCards: string[] = ["1", "1", "2", "2", "3", "3", "4", "4", "5", "5", "6", "6", "7", "7", "8", "8", "9", "9", "10", "10",
         "11", "11", "12", "12", "13", "13", "14", "14", "15", "15", "16", "16", "17", "17", "18", "18", "19", "19", "20", "20", "21", "21", "22", "22",
-        "23", "23", "24", "24", "25", "25", "26", "26", "27", "27", "28", "28"];
+        "23", "23", "24", "24", "25", "25"];
     let newCards: string[] = [];
+    let body: HTMLElement;
     let colorSelector: HTMLInputElement;
     let cardColor: string;
+    let bodyColorSelector: HTMLInputElement; 
+    let fontColor: string;
+    let fontColorSelector: HTMLInputElement;
+    let sizeSelector: HTMLInputElement;
+    let cardSize: string;
+    let chosenCards: string[] = [];
+    let stepperSelector: HTMLInputElement;
+    let cardFont: string;
+    let minutes: number = 0;
+    let seconds: number = 0;
 
     function handleLoad(_event: Event): void {
         document.getElementById("StartGame").addEventListener("click", startGame);
         colorSelector = <HTMLInputElement>document.querySelector("#color");
+        bodyColorSelector = <HTMLInputElement>document.querySelector("#bodycolor");
+        fontColorSelector = <HTMLInputElement>document.querySelector("#fontcolor");
+        sizeSelector = <HTMLInputElement>document.querySelector("#slider");
+        stepperSelector = <HTMLInputElement>document.querySelector("#stepper");
+        body = document.querySelector("body");
+    }
+
+    function radioEntry(): void {
+        let formData: FormData = new FormData(document.forms[0]);
+        if (formData.get("radio") != null && formData.get("radio") != undefined) {
+            cardFont = (formData.get("radio") + "");
+        }
     }
     function startGame(): void {
         cardColor = colorSelector.value;
-        shuffleArray(allCards);
+        body.style.backgroundColor = bodyColorSelector.value;
+        fontColor = fontColorSelector.value;
+        cardSize = sizeSelector.value;
+        chooseCards();
+        radioEntry();
+        shuffleArray(chosenCards);
+        body.removeChild(document.querySelector("#menu"));
+        setTimer();
     }
+
+
+    function chooseCards(): void {
+        for (let i: number = 0; i < parseInt(stepperSelector.value); i++) {
+            chosenCards.push(allCards[i]);
+        }
+        console.log(chosenCards);
+
+    }
+
 
     function shuffleArray(_allCards: string[]): string[] {
         let randomCard: number;
@@ -33,6 +73,8 @@ namespace MemoryGame {
             _allCards.splice(randomCard, 1);
 
         }
+        console.log(newCards);
+
         return newCards;
     }
 
@@ -40,12 +82,14 @@ namespace MemoryGame {
         let card: HTMLDivElement = document.createElement("div");
         card.style.backgroundColor = cardColor;
         card.style.color = cardColor;
+        card.style.padding = cardSize + "px";
+        card.style.fontFamily = cardFont;
+        console.log(cardFont);
         card.addEventListener("click", () => { showCard(card); });
         let field: HTMLElement = document.getElementById("cardField");
         card.innerHTML = _cardValue;
         card.className = "Card " + _cardValue;
         field.appendChild(card);
-
     }
 
     let oneActiveCard: boolean = false;
@@ -53,7 +97,9 @@ namespace MemoryGame {
 
 
     function showCard(_card: HTMLDivElement): void {
-        _card.style.color = "white";
+        _card.style.color = fontColor;
+        _card.style.pointerEvents = "none";
+        _card.style.backgroundColor = "blue";
         currentCards.push(_card);
         if (oneActiveCard == false) {
             oneActiveCard = true;
@@ -66,20 +112,41 @@ namespace MemoryGame {
     }
 
     function hideCards(): void {
+        currentCards[0].style.backgroundColor = cardColor;
+        currentCards[1].style.backgroundColor = cardColor;
         currentCards[0].style.color = cardColor;
         currentCards[1].style.color = cardColor;
         console.log(currentCards);
         if (currentCards[0].className == currentCards[1].className) {
             currentCards[0].style.visibility = "hidden";
             currentCards[1].style.visibility = "hidden";
+            currentCards[0].setAttribute("deleted", "deleted");
+            currentCards[1].setAttribute("deleted", "deleted");
+            if (document.querySelectorAll("div div").length == document.querySelectorAll("[deleted = 'deleted']").length) {
+                alert("Du hast gewonnen! In " + minutes + " Minuten und " + seconds + " Sekunden!");
+                location.reload();
+            }
         }
+        currentCards[0].style.pointerEvents = "";
+        currentCards[1].style.pointerEvents = "";
         currentCards = [];
         document.querySelector("body").style.pointerEvents = "";
     }
+
+    function setTimer(): void {
+        setInterval(timer, 1000);
+    }
+    function timer(): void {
+        seconds++;
+        if (seconds == 60) {
+            seconds = 0;
+            minutes++;
+            }
+        document.querySelector("#timer").innerHTML = minutes + ":" + seconds;
+        }
 
     function startTimer(): void {
         hideCards();
     }
 
 }
-// Work in Progress
